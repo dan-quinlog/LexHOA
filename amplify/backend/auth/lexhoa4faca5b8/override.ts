9cb57d927e36c6cf828cccc77810ac2bf3475c79
Daniel <dan.quinlog@gmail.com>
2026-08-18 10:59:57 -0600

    Allow staging OAuth redirects

--- amplify/backend/auth/lexhoa4faca5b8/override.ts
+++ amplify/backend/auth/lexhoa4faca5b8/override.ts
@@ -1,14 +1,13 @@
 import { AmplifyAuthCognitoStackTemplate, AmplifyProjectInfo } from '@aws-amplify/cli-extensibility-helper';
 
 export function override(resources: AmplifyAuthCognitoStackTemplate, amplifyProjectInfo: AmplifyProjectInfo) {
-  if (amplifyProjectInfo.envName !== 'dev') {
-    return;
-  }
+  const redirectUrls = amplifyProjectInfo.envName === 'dev'
+    ? ['http://localhost:3000/', 'https://dev.lexingtoncommons-weco.com/']
+    : amplifyProjectInfo.envName === 'staging'
+      ? ['http://localhost:3000/', 'https://staging.lexingtoncommons-weco.com/']
+      : null;
 
-  const redirectUrls = [
-    'http://localhost:3000/',
-    'https://dev.lexingtoncommons-weco.com/',
-  ];
+  if (!redirectUrls) return;
 
   resources.userPoolClient.addPropertyOverride('CallbackURLs', redirectUrls);
   resources.userPoolClient.addPropertyOverride('LogoutURLs', redirectUrls);
